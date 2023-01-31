15a79548fb32895af428da10f39d84ab9e6174bc
Mike G <kukozi47@gmail.com>
2023-01-31 20:51:38 +0300

    add openweather api files

---
+++ openweather_api/api.py
@@ -0,0 +1,5 @@
+# file api.py
+
+api_key = 'db3ead0f998f033cbb02b711a32a5885'
+default_city = 'Petersburg,RU'
+uri = 'https://api.openweathermap.org/data/2.5/weather'
---
+++ openweather_api/location.py
@@ -0,0 +1,7 @@
+# file location.py
+
+city = 'Saint-Petersburg'
+country = 'Russia'
+region = 'RU'
+lon = 59.903554
+lat = 30.468828
