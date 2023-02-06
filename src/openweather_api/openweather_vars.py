fd211cd5ea61ff76e5ec39d1238f88d3ccf3417c
Mike G <kukozi47@gmail.com>
2023-02-06 13:10:23 +0300

    separate and move openweather parameter

--- src/openweather_api/openweather_vars.py
+++ src/openweather_api/openweather_vars.py
@@ -1,5 +1,6 @@
 # file api.py
 
-api_key = 'db3ead0f998f033cbb02b711a32a5885'
 default_city = 'Petersburg,RU'
+#uri = 'https://api.openweathermap.org/data/2.5/forecast/daily'
 uri = 'https://api.openweathermap.org/data/2.5/weather'
+exclude = 'minutely,hourly,alerts'
